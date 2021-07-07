bb43ef78f8612dfd865d018b1df388a9f126a7a6
Shubhamb1 <shubhambirle496@gmail.com>
2021-07-07 17:03:10 +0530

    Main method updated

--- main.java
+++ main.java
@@ -3,7 +3,9 @@ class Main{
      public static void main(String args[])
      {
           System.out.println("Hello World");
-
+          System.out.println("1234567890");
+          System.out.println("Hello World");
+          System.out.println("**************");
 
       }
 
